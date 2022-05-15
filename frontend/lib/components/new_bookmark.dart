8c356c8de95f6b412bff9cfcc28d8e816e3deda3
charitarthchugh <37895518+charitarthchugh@users.noreply.git>
2022-05-15 10:53:16 -0400

    Add the UI for a new bookmark

---
+++ frontend/lib/components/new_bookmark.dart
@@ -0,0 +1,38 @@
+import "package:flutter/material.dart";
+import 'package:flutter_nord_theme/flutter_nord_theme.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class NewBookmark extends StatelessWidget {
+  const NewBookmark({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          primary: NordColors.polarNight.lightest,
+          maximumSize: const Size(115, 42),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(90))),
+      onPressed: (() {}),
+      child: Center(
+        child: Row(
+          children: [
+            Icon(
+              Icons.add,
+              color: NordColors.snowStorm.lightest,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left:8.0),
+              child: Text(
+                "New",
+                style: GoogleFonts.getFont("Poppins",
+                    textStyle: Theme.of(context).textTheme.titleMedium,
+                    color: NordColors.snowStorm.lightest),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
