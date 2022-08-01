e61c3535c0e95f3e1d4b97f370ef2552654fefee
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2022-08-01 05:53:45 -0400

    style(backend): format with isort

--- backend/tests/test_deamon.py
+++ backend/tests/test_deamon.py
@@ -1,12 +1,11 @@
 from pathlib import Path
 
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
 from bookie_backend.bookied.data.models import Base
 from bookie_backend.bookied.deamon import app, get_db
 from bookie_backend.bookied.utils import bookmark_parser
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 path = Path("test.sqlite")
 if path.exists():
