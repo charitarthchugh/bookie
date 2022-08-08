94435e56679ad79a21c4540504f78f1304fcb7a1
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2022-08-08 18:53:34 -0400

    fix(cli): :white_check_mark: fix rename change not present in tests

--- backend/tests/test_cli_daemon.py
+++ backend/tests/test_cli_daemon.py
@@ -14,19 +14,19 @@ def get_running_proc(proc_name: str) -> Optional[psutil.Process]:
 
 
 def test_start():
-    output = runner.invoke(app, ["deamon", "start"])
+    output = runner.invoke(app, ["daemon", "start"])
     assert get_running_proc("bookied")
 
 
 def test_restart():
     previous_instance = get_running_proc("bookied")
-    output = runner.invoke(app, ["deamon", "restart"])
+    output = runner.invoke(app, ["daemon", "restart"])
     assert output.exit_code == 0
     current_instance = get_running_proc("bookied")
     assert previous_instance != current_instance
 
 
 def test_stop():
-    output = runner.invoke(app, ["deamon", "stop"])
+    output = runner.invoke(app, ["daemon", "stop"])
     assert output.exit_code == 0
     assert get_running_proc("bookied") is None
