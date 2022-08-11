826a4d7a8b4da31bffdad7a1cdbfebccfa47474f
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2022-08-11 16:29:29 -0400

    fix: :bug: fix test.dart import

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:frontend/main.dart';
+import 'package:bookief/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
