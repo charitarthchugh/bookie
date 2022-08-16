937a765ab093d25b0ba153017e081075a5fff6d7
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2022-08-16 19:26:04 -0400

    refactor(backend): :recycle: fix rename change in tests
    forgot to  fix tests in accordance to rename

--- backend/tests/test_daemon.py
+++ backend/tests/test_daemon.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from bookie_backend.bookied.data.models import Base
-from bookie_backend.bookied.deamon import app, get_db
+from bookie_backend.bookied.daemon import app, get_db
 from bookie_backend.bookied.utils import bookmark_parser
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
