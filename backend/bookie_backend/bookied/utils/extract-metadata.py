04f6e9fd7a423e6b09b5f6fcc52d8da933d5659d
Charitarth Chugh <charitarth.chughg@gmail.com>
2023-08-06 12:54:34 -0400

    Add metadata extraction, fix hashing

--- backend/bookie_backend/bookied/utils/extract-metadata.py
+++ backend/bookie_backend/bookied/utils/extract-metadata.py
@@ -1,15 +1,50 @@
 import hashlib
+import requests
+from bs4 import BeautifulSoup
+from typing import Optional
+from io import BytesIO
+import urllib
 
 
+def extract_metadata(url) -> Optional[dict]:
+    # if url is None:
+    #     return
 
-def hash(input)->str:
-    if input is None:
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        title = soup.find("title")
+        description = soup.find("meta", property="og:description")
+        # Get base url
+        base_url = urllib.parse.urlparse(url).hostname
+        req = requests.get(f"https://favicongrabber.com/api/grab/{base_url}")
+        fav_url = req.json()["icons"][0]["src"]
+        favicon = requests.get(fav_url).content
+
+        return {
+            "title": title.string if title else None,
+            "description": description["content"] if description else None,
+            "favicon": favicon if favicon else None,
+            "url": url,
+        }
+    except Exception as e:
+        print(e)
+        return None
+
+
+def md5(content) -> Optional[str]:
+    if content is None:
         return
-
+    content = BytesIO(content)
     md5 = hashlib.md5()
-    md5.update(input)
+    md5.update(content.read())
     return md5.hexdigest()
 
+
 if __name__ == "__main__":
-    hash_icon = "https://www.lfaticon.com/free-icon/url_1078454"
-    print(hash(hash_icon))
+    meta = extract_metadata("https://www.youtube.com/watch?v=9bZkp7q19f0")
+    from pathlib import Path
+    # save favicon
+    favicon = meta["favicon"]
+    if favicon:
+        print(md5(favicon))
